aaeaa42737f3d0152a3d423e92a5fb7ba0fa4157
Alex Santana <alexandresntn@yahoo.com>
2021-07-01 17:52:41 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var displayDiv = document.querySelector('#display')
+console.log(displayDiv);
+var num1 = 0
+var num2 = 0
+
+function press(element){
+    displayDiv.innerText = element
+}
+// Grab the displayed number and store it
+// Grab the operator
+// Remove the number from the display
+// Grab and store the new number
+// Perform the operation
+// Store and display the final number on the first var
