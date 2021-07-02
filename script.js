57fee730df80be761b003795b1802119721e050f
Alex Santana <alexandresntn@yahoo.com>
2021-07-01 20:41:01 -0500

    Made it work

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 var displayDiv = document.querySelector('#display')
 console.log(displayDiv);
-var num1 = 0
-var num2 = 0
+var num1 = ""
+var op = ""
+var num2 = ""
 
-function press(element){
-    displayDiv.innerText = element
+function press(element) {
+    firstNum = element.toString();
+    displayDiv.innerText = displayDiv.innerHTML + firstNum;
 }
+
+function clr() {
+    displayDiv.innerText = ""
+}
+
+function setOP(element) {
+    num1 = displayDiv.innerText
+    displayDiv.innerText = ''
+    op = element
+}
+
+function calculate() {
+    var num2 = displayDiv.innerText
+    var answer = 0
+    if (op == "+") {
+        answer = parseFloat(num1) + parseFloat(num2);
+        displayDiv.innerText = answer;
+    }
+    if (op == "-") {
+        answer = parseFloat(num1) - parseFloat(num2);
+        displayDiv.innerText = answer
+    }
+    if (op == "*") {
+        answer = parseFloat(num1) * parseFloat(num2);
+        displayDiv.innerText = answer
+    }
+    if (op == "/") {
+        answer = parseFloat(num1) / parseFloat(num2);
+        displayDiv.innerText = answer
+    }
+}
+
 // Grab the displayed number and store it
 // Grab the operator
 // Remove the number from the display
